69744b1843288827912279d7880121c1c76380cd
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-09-16 09:38:58 -0400

    Learning to Code in Python

---
+++ app.py
@@ -0,0 +1,5 @@
+# FreeCodeCamp Youtube Tutorial: https://www.youtube.com/watch?v=rfscVS0vtbw&t=402s. Learning as much coding as I can just cause I love to code.
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
